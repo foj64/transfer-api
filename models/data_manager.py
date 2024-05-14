88aad65b9e41b09f46aa11103a3ce38713cc641d
Filipe Oliveira Joao <foj64@hotmail.com>
2024-05-13 22:11:17 -0300

    Creating DataManager rules

--- models/data_manager.py
+++ models/data_manager.py
@@ -1,6 +1,11 @@
 class DataManager:
-    def __init__(self):
-        self.data = {}
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(DataManager, cls).__new__(cls, *args, **kwargs)
+            cls._instance.data = {}
+        return cls._instance
 
     def add_amount(self, id, amount):
         if id in self.data:
@@ -8,4 +13,24 @@ class DataManager:
         else:
             self.data[id] = amount
         return self.data[id]
+
+    def transfer_amount(self, origin, destination, amount):
+        if origin not in self.data or self.data[origin] < amount:
+            return False, 0, 0
+
+        self.data[origin] -= amount
+        self.data[destination] = self.data.get(destination, 0) + amount
+        return True, self.data[origin], self.data[destination]
+
+    def withdraw_amount(self, id, amount):
+        if id not in self.data or self.data[id] < amount:
+            return False, 0
+        self.data[id] -= amount
+        return True, self.data[id]
+
+    def reset_data(self):
+        self.data = {}
+
+    def get_balance(self, id):
+        return self.data.get(id)
         
